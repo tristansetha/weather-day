8d888a96433b42a4e4685488b7ea71eee17232b9
ts <tristansetha@gmail.com>
2019-02-05 15:58:21 -0800

    created user input to create a new object for the current day of the week

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
-function Weather(Temp)
+// var temperature = ["clear", "sunny", ]
 
+
+
+function Day(day) {
+    this.day = day
+}
+
+Day.prototype.fiveDayforecast = function(days) {
+    var days = []
+    if (this.day == "monday"){
+        days.push("monday", "tuesday", "wednesday", "thursday", "friday", "saturday");
+    } else if (this.day == "tuesday") { 
+        days.push("tuesday", "wednesday", "thursday", "friday", "saturday", "sunday");
+    } else if (this.day == "wednesday") {
+        days.push("wednesday", "thursday", "friday", "saturday", "sunday", "monday")
+    }
+}
 
 
 
@@ -7,5 +23,12 @@ function Weather(Temp)
 
 
 $(document).ready(function() {
+    $("form#form-weather").submit(function(event){
+        event.preventDefault();
+        var inputtedDay = $("select#selectDay").val();
 
+        var newDay = new Day(inputtedDay)
+
+        alert(newDay);
+    });
 });
