ab7621e684f1091cc55747b63dacd01eafd3eced
ts <tristansetha@gmail.com>
2019-02-05 21:53:08 -0800

    functioning randomized weather for each day with class status

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,37 @@
-// var temperature = ["clear", "sunny", ]
-
 function Day(day, temp, precip) {
     this.day = day,
     this.temp = temp,
-    this.precip = precip
+    this.precip = precip,
+    this.classStatus = undefined
 }
 
+Day.prototype.classStatus2 = function() {
+    if (this.precip == "blizzard" || "snow" || "oh-snap a tornado"){
+        return this.classStatus = "closed";
+    } else {
+        return this.classStatus = "open";
+    }
+        
+}
 
+Day.prototype.addWeek = function() {
+    this.week.push(Day);
+}
 
 $(document).ready(function() {
     $("form#form-weather").submit(function(event){
         event.preventDefault();
+        
+        var inputtedDay = $("select#selectDay").val();
 
         var tempArray = [...Array(70).keys()];
         var precipArray = ["blizzard", "snow", "cloudy", "light-rain", "rain", "heavy-rain", "sunny", "windy", "oh-snap a tornado", "clear-sky"]
         
-        var randomTemp = tempArray[Math.floor(Math.random() * tempArray.length)];
-        var randomPrecip = precipArray[Math.floor(Math.random() * precipArray.length)];
+        tempArray[Math.floor(Math.random() * tempArray.length)];
+        precipArray[Math.floor(Math.random() * precipArray.length)];
         
         var dayArray = [];
-        var newDay = ["var day1", "var day2", "var day3", "var day4", "var day5"];
-       
-        var inputtedDay = $("select#selectDay").val();
-       
+        
         if (inputtedDay == "monday"){
             dayArray.push("monday", "tuesday", "wednesday", "thursday", "friday", "saturday");
         } else if (inputtedDay == "tuesday") { 
@@ -35,11 +44,52 @@ $(document).ready(function() {
             dayArray.push("friday", "saturday", "sunday", "monday", "tuesday", "wednesday");
         }
 
+        var newDay0 = new Day(dayArray[0], tempArray[Math.floor(Math.random() * tempArray.length)], precipArray[Math.floor(Math.random() * precipArray.length)]);
+        var newDay1 = new Day(dayArray[1], tempArray[Math.floor(Math.random() * tempArray.length)], precipArray[Math.floor(Math.random() * precipArray.length)]);
+        var newDay2 = new Day(dayArray[2], tempArray[Math.floor(Math.random() * tempArray.length)], precipArray[Math.floor(Math.random() * precipArray.length)]);
+        var newDay3 = new Day(dayArray[3], tempArray[Math.floor(Math.random() * tempArray.length)], precipArray[Math.floor(Math.random() * precipArray.length)]);
+        var newDay4 = new Day(dayArray[4], tempArray[Math.floor(Math.random() * tempArray.length)], precipArray[Math.floor(Math.random() * precipArray.length)]);
+        var newDay5 = new Day(dayArray[5], tempArray[Math.floor(Math.random() * tempArray.length)], precipArray[Math.floor(Math.random() * precipArray.length)]);
 
-        for (i = 0; i < dayArray.length; i++) {
-           newDay[i] = new Day(dayArray[i], randomTemp, randomPrecip);  
-           $("ol#show-days").append("<li>" + dayArray[i] + ":" + " " + randomTemp + randomPrecip + "</li>");
-        }
+        // var status0 = newDay0.classStatus
+        // var status1 = newDay1.classStatus
+        // var status2 = newDay2.classStatus
+        // var status3 = newDay3.classStatus
+        // var status4 = newDay4.classStatus
+        // var status5 = newDay5.classStatus
+
+
+        $("li#day0").text(newDay0.day);
+        $("li#day0-temp").text(newDay0.temp);
+        $("li#day0-precip").text(newDay0.precip);
+        $("li#day0-classStatus").text(newDay0.classStatus2);
+
+        $("li#day1").text(newDay1.day);
+        $("li#day1-temp").text(newDay1.temp);
+        $("li#day1-precip").text(newDay1.precip);
+        $("li#day1-classStatus").text(newDay1.classStatus2);
+
+        $("li#day2").text(newDay2.day);
+        $("li#day2-temp").text(newDay2.temp);
+        $("li#day2-precip").text(newDay2.precip);
+        $("li#day2-classStatus").text(newDay2.classStatus2);
+        
+        $("li#day3").text(newDay3.day);
+        $("li#day3-temp").text(newDay3.temp);
+        $("li#day3-precip").text(newDay3.precip);
+        $("li#day3-classStatus").text(newDay3.classStatus2);
+
+        $("li#day4").text(newDay4.day);
+        $("li#day4-temp").text(newDay4.temp);
+        $("li#day4-precip").text(newDay4.precip);
+        $("li#day4-classStatus").text(newDay4.classStatus2);
+
+        $("li#day5").text(newDay5.day);
+        $("li#day5-temp").text(newDay5.temp);
+        $("li#day5-precip").text(newDay5.precip);
+        $("li#day5-classStatus").text(newDay5.classStatus2);
+
+
 
         // var fiveDays = newDay;
 
@@ -50,4 +100,5 @@ $(document).ready(function() {
     });
 });
 
-// "<br/>" + randomTemp + "<br/>" + randomPrecip + 
+// "<br/>" +         tempArray[Math.floor(Math.random() * tempArray.length)];
+//  + "<br/>" + precipArray[Math.floor(Math.random() * precipArray.length)]; + 
