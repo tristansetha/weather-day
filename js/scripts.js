3b1719dbaad4d65317b053aff33c4a181a558e73
ts <tristansetha@gmail.com>
2019-02-05 16:54:51 -0800

    able to output what the next five days are  based on the user input

--- js/scripts.js
+++ js/scripts.js
@@ -6,29 +6,33 @@ function Day(day) {
     this.day = day
 }
 
-Day.prototype.fiveDayforecast = function(days) {
-    var days = []
+Day.prototype.fiveDayforecast = function() {
+    var nextDays = [ ]
     if (this.day == "monday"){
-        days.push("monday", "tuesday", "wednesday", "thursday", "friday", "saturday");
+        nextDays.push("monday", "tuesday", "wednesday", "thursday", "friday", "saturday");
     } else if (this.day == "tuesday") { 
-        days.push("tuesday", "wednesday", "thursday", "friday", "saturday", "sunday");
+        nextDays.push("tuesday", "wednesday", "thursday", "friday", "saturday", "sunday");
     } else if (this.day == "wednesday") {
-        days.push("wednesday", "thursday", "friday", "saturday", "sunday", "monday")
+        nextDays.push("wednesday", "thursday", "friday", "saturday", "sunday", "monday");
+    } else if (this.day == "thursday") {
+        nextDays.push("thursday", "friday", "saturday", "sunday", "monday", "tuesday");
+    } else if (this.day == "friday") {
+        nextDays.push("friday", "saturday", "sunday", "monday", "tuesday", "wednesday");
     }
-}
-
-
-
-
-
+    return nextDays;
+};
 
 $(document).ready(function() {
     $("form#form-weather").submit(function(event){
         event.preventDefault();
         var inputtedDay = $("select#selectDay").val();
 
-        var newDay = new Day(inputtedDay)
+        var newDay = new Day(inputtedDay);
+        var fiveDays = newDay.fiveDayforecast();
 
-        alert(newDay);
+        for (i = 0; i < fiveDays.length; i++) {
+            $("ol#show-days").append("<br/><li>" + fiveDays[i] + "</li>");
+        }
+        
     });
 });
